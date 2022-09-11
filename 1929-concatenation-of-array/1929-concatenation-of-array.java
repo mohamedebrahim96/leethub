d3ed6711d8878587f43797b8845f7997e57085cc
Mohamed Ebrahim <ebrahimm131@gmail.com>
2022-09-11 12:05:30 +0400

    Time: 1 ms (98.85%), Space: 42.8 MB (94.65%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.java
+++ 1929-concatenation-of-array/1929-concatenation-of-array.java
@@ -4,8 +4,8 @@ class Solution {
 
         
         for(int i=0; i<nums.length; i++){
-            ans[i] = nums[i];
-            ans[i + nums.length] = ans[i];
+           // ans[i] = nums[i];
+            ans[i + nums.length] = ans[i] = nums[i];
         
         }
         
