e3aeaec3794d89161a54de77d2f0df08364002fd
Mohamed Ebrahim <ebrahimm131@gmail.com>
2022-09-11 12:06:53 +0400

    Time: 2 ms (43.39%), Space: 50.2 MB (31.67%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.java
+++ 1929-concatenation-of-array/1929-concatenation-of-array.java
@@ -4,12 +4,14 @@ class Solution {
 
         
         for(int i=0; i<nums.length; i++){
-           // ans[i] = nums[i];
             ans[i + nums.length] = ans[i] = nums[i];
-        
         }
-        
-        
         return ans;
     }
 }
+
+
+/*
+*Time complixety = O(n)
+*Space Complixity = O(1)
+*/
