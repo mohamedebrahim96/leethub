39365c49feff766d6e6456671bd3ad074441e178
Mohamed Ebrahim <ebrahimm131@gmail.com>
2022-09-25 20:21:22 +0400

    Time: 2 ms (17.29%), Space: 43 MB (69.79%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.java
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -8,12 +8,10 @@ class Solution {
             for(int j=0;j<accounts[i].length; j++){
                 sum_small += accounts[i][j];
             }
-            if(max<=sum_small){
-                max = sum_small;
-                sum_small = 0;
-            }else{
-                sum_small = 0;
-            }
+            max = Math.max(max,sum_small);
+            sum_small = 0;
+
+           
            
         }
         
