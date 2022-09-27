989acf43a992259ea2010294fff5e406a0edf52e
Mohamed Ebrahim <ebrahimm131@gmail.com>
2022-09-27 12:16:05 +0400

    Time: 1 ms (98.49%), Space: 40.4 MB (98.16%) - LeetHub

---
+++ 771-jewels-and-stones/771-jewels-and-stones.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int count = 0;
+        Set<Character> set = new HashSet<>();
+        for (char c : jewels.toCharArray()) {
+            set.add(c);
+        }
+        for (char c : stones.toCharArray()) {
+            if (set.contains(c)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
