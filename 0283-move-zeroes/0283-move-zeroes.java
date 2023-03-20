2eeb7b4e959f6925426651582f6d14491fbb6715
Mohamed Ebrahim <ebrahimm131@gmail.com>
2023-03-20 09:20:34 +0400

    Time: 1 ms (100.00%), Space: 44.3 MB (14.55%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int[] arr = new int[nums.length];
+        int j = 0;
+        for(int i = 0;i<nums.length;i++){
+            if(nums[i] != 0){
+                arr[j] = nums[i];
+                j++;
+            }
+        }
+        for(int i = 0;i<nums.length;i++){
+            nums[i] = arr[i]; 
+        }
+    }
+}
