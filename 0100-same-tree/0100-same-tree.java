f238e908f8cc702eb17b4a6b76508e865c92da85
Mohamed Ebrahim <ebrahimm131@gmail.com>
2023-04-13 11:35:49 +0400

    Time: 0 ms (100.00%), Space: 40.6 MB (13.88%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -13,15 +13,45 @@
  *     }
  * }
  */
-class Solution {
-    public boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null && q == null) {
+class Solution 
+{
+    // it will be updated to false if we find size difference, or diff value
+    boolean value = true;
+
+    public boolean isSameTree(TreeNode p, TreeNode q) 
+    {
+        if(p == null && q == null)
             return true;
-        } else if (p != null && q != null) {
-            return (p.val == q.val) && isSameTree(p.right, q.right) 
-                && isSameTree(p.left, q.left);
-        } else {
+
+        //one tree is null, while other is not
+        else if(p == null || q == null)
             return false;
+
+        traverse(p, q);    
+        return value;           
+    }
+
+    public void traverse (TreeNode ptr1, TreeNode ptr2)
+    {
+        //same size if verified
+        if(ptr1 == null && ptr2 == null)
+            return;
+
+        // if one tree is diff then the other in size
+        else if(ptr1 == null || ptr2 == null)
+        {
+            value = false;
+            return;
         }
+
+        traverse(ptr1.left, ptr2.left);
+
+        if(ptr1.val != ptr2.val)
+        {
+            value = false;
+            return;
+        }
+
+        traverse(ptr1.right, ptr2.right);
     }
 }
