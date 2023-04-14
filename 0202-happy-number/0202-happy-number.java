70d873ffd0dcf8768402600b693d952f44a01ece
Mohamed Ebrahim <ebrahimm131@gmail.com>
2023-04-14 12:39:10 +0400

    Time: 1 ms (91.55%), Space: 39.7 MB (66.79%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,23 +1,28 @@
 class Solution {
     public boolean isHappy(int n) {
-        List<Integer> arr = new ArrayList<Integer>();
-        while(n>0){
+        // Create a set to keep track of the numbers that have already been seen
+        Set<Integer> seen = new HashSet<>();
+        
+        // Loop until either n becomes 1 (in which case n is a happy number) or n is seen again (in which case n is not a happy number)
+
+        while (n != 1 && !seen.contains(n)) {
+            // Add n to the set of seen numbers
+            seen.add(n);
+            
+            // Compute the sum of the squares of the digits of n
             int sum = 0;
-            while(n>0){
-                int k = n%10;
-                sum+= k*k;
-                n/=10;
+            while (n != 0) {
+                int digit = n % 10;
+                sum += digit * digit;
+                n /= 10;
             }
-            if(arr.contains(sum)){
-                return false;
-            }else{
-                arr.add(sum);
-            }
-            if(sum==1){
-                return true;
-            }
+            
+            // Update n with the sum of the squares of its digits
+            
             n = sum;
         }
-        return false;
+        
+        // If n is 1, it is a happy number; otherwise, it is not
+        return n == 1;
     }
 }
