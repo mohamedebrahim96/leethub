a7f3c80c06436c4d46ef80024e8f32f6c5c1f59f
Mohamed Ebrahim <ebrahimm131@gmail.com>
2023-04-17 12:59:15 +0400

    Time: 0 ms (100.00%), Space: 40.3 MB (94.61%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,17 +1,16 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        // trim the trailing spaces
-        int p = s.length() - 1;
-        while (p >= 0 && s.charAt(p) == ' ') {
-            p--;
-        }
-
-        // compute the length of last word
-        int length = 0;
-        while (p >= 0 && s.charAt(p) != ' ') {
-            p--;
-            length++;
-        }
-        return length;
+          int count=0;
+          String z=s.trim();
+for(int i=z.length()-1;i>=0;i--)
+{
+    if(z.charAt(i)==' ')
+    {
+        break;
+    }        
+        else{
+        count++;
     }
-}
+   }
+  return count; 
+}}
