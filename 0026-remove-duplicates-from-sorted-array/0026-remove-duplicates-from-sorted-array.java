6830c94e395db96945d472e2b617f58dcaf32203
Mohamed Ebrahim <ebrahimm131@gmail.com>
2023-04-18 09:11:47 +0400

    Time: 1 ms (99.96%), Space: 43.7 MB (89.11%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,15 +1,12 @@
 class Solution {
-     public int removeDuplicates(int[] nums) {
-        int insertIndex = 1;
-        for(int i = 1; i < nums.length; i++){
-            // We skip to next index if we see a duplicate element
-            if(nums[i - 1] != nums[i]) {
-                /* Storing the unique element at insertIndex index and incrementing
-                   the insertIndex by 1 */
-                nums[insertIndex] = nums[i];     
-                insertIndex++;
+    public int removeDuplicates(int[] nums) {
+        int i = 0;
+        for(int n: nums){
+            if(i == 0 || n > nums[i - 1]){
+                nums[i] = n;
+                i++;
             }
         }
-        return insertIndex;
+        return i;
     }
 }
