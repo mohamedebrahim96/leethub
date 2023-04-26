ebeb0f9b479e8177d47a692971b13eafe9f0c945
Mohamed Ebrahim <ebrahimm131@gmail.com>
2023-04-26 11:29:16 +0400

    Time: 0 ms (100.00%), Space: 42.3 MB (62.87%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -14,13 +14,13 @@
  * }
  */
 class Solution {
-  public int maxDepth(TreeNode root) {
-    if (root == null) {
-      return 0;
-    } else {
-      int left_height = maxDepth(root.left);
-      int right_height = maxDepth(root.right);
-      return java.lang.Math.max(left_height, right_height) + 1;
+    public int maxDepth(TreeNode root) {
+        // Base Condition
+        if(root == null) return 0;
+        // Hypothesis
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        // Induction
+        return Math.max(left, right) + 1;
     }
-  }
 }
