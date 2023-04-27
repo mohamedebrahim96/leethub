3226f20fdca3847d53ef20d2632ca8494d49bc7e
Mohamed Ebrahim <ebrahimm131@gmail.com>
2023-04-27 13:09:28 +0400

    Time: 0 ms (100.00%), Space: 41 MB (38.71%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -14,25 +14,32 @@
  * }
  */
 class Solution {
-   public boolean isSymmetric(TreeNode root) {
-  if (root == null) return true;
-  Stack<TreeNode> stack = new Stack<>();
-  stack.push(root.left);
-  stack.push(root.right);
+    public boolean isSymmetric(TreeNode root) {
+        if(root==null)
+            return false;
+        if(root.left==null &&root.right==null){
+            return true;
+        }
+        
+        if(root.left!=null && root.right!=null && root.left.val==root.right.val){
+            return mirror(root.left,root.right);
+        }
+        return false;
+    }
 
-  while (!stack.isEmpty()) {
-   TreeNode n1 = stack.pop();
-   TreeNode n2 = stack.pop();
+    public boolean mirror(TreeNode left,TreeNode right){
+        if(left==null && right==null)
+            return true;
+        if(left==null || right==null )
+            return false;
+        
+        if(left.left!=null && right.right!=null && left.left.val!=right.right.val){
+            return false;
+        }
+        if(left.right!=null && right.left!=null && left.right.val!=right.left.val)
+            return false;
 
-   if (n1 == null && n2 == null) continue;
-   if (n1 == null || n2 == null || n1.val != n2.val) return false;
-
-   stack.push(n1.left);
-   stack.push(n2.right);
-   stack.push(n1.right);
-   stack.push(n2.left);
-  }
-
-  return true;
- }
+        return mirror(left.left,right.right) && mirror(left.right,right.left);
+    
+    }
 }
