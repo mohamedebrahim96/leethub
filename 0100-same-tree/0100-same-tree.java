8d5c62e7ce78d8b3fb6a353a95c8f6599ef7a60b
Mohamed Ebrahim <ebrahimm131@gmail.com>
2023-04-28 12:17:59 +0400

    Time: 0 ms (100.00%), Space: 40.4 MB (21.16%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -13,45 +13,50 @@
  *     }
  * }
  */
-class Solution 
-{
-    // it will be updated to false if we find size difference, or diff value
-    boolean value = true;
-
-    public boolean isSameTree(TreeNode p, TreeNode q) 
-    {
-        if(p == null && q == null)
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        // condition checking for root nodes
+        if (p == null && q == null) {
             return true;
-
-        //one tree is null, while other is not
-        else if(p == null || q == null)
+        }
+        if ((p == null && q != null) || (p != null && q == null)) {
             return false;
-
-        traverse(p, q);    
-        return value;           
-    }
-
-    public void traverse (TreeNode ptr1, TreeNode ptr2)
-    {
-        //same size if verified
-        if(ptr1 == null && ptr2 == null)
-            return;
-
-        // if one tree is diff then the other in size
-        else if(ptr1 == null || ptr2 == null)
-        {
-            value = false;
-            return;
         }
 
-        traverse(ptr1.left, ptr2.left);
+        Stack<TreeNode> st1 = new Stack<>();
+        Stack<TreeNode> st2 = new Stack<>();
+        st1.push(p);
+        st2.push(q);
 
-        if(ptr1.val != ptr2.val)
-        {
-            value = false;
-            return;
+        while (!st1.empty() && !st2.empty()) {
+            TreeNode n1 = st1.pop();
+            TreeNode n2 = st2.pop();
+
+            // check if values are equal
+            if (n1.val != n2.val) {
+                return false;
+            }
+            // condition checking for children nodes
+            if ((n1.left != null && n2.left == null) || (n1.left == null && n2.left != null)) {
+                return false;
+            }
+            if ((n1.right != null && n2.right == null) || (n1.right == null && n2.right != null)) {
+                return false;
+            }
+
+            // add the children to the stacks
+            if (n1.left != null && n2.left != null) {
+                st1.push(n1.left);
+                st2.push(n2.left);
+            }
+            if (n1.right != null && n2.right != null) {
+                st1.push(n1.right);
+                st2.push(n2.right);
+            }
         }
 
-        traverse(ptr1.right, ptr2.right);
+        // any dissimilarity if encountered above, would've returned false already
+        // no dissimilarity encountered means trees are same, so return true
+        return true;
     }
 }
