5f3c6c9472b0475e7f8dfdb728375811b7751f99
Mohamed Ebrahim <ebrahimm131@gmail.com>
2023-05-08 11:24:48 +0400

    Time: 0 ms (100.00%), Space: 39.8 MB (13.29%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.java
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -1,11 +1,26 @@
 class Solution {
   public boolean isPerfectSquare(int num) {
-    if (num < 2) return true;
+	if (num == 1) return true;
+	if (num < 4) return false;
 
-    long x = num / 2;
-    while (x * x > num) {
-      x = (x + num / x) / 2;
-    }
-    return (x * x == num);
-  }
+	int end = num / 2;
+	int start = 0;
+	int mid;
+
+	while (start <= end) {
+		mid = (end - start) / 2 + start;
+
+		double calculated = Math.pow(mid, 2);
+		if (calculated == num) return true;
+
+		if (calculated < num) {
+			start = mid + 1;
+		} else {
+			end = mid - 1;
+		}
+
+	}
+
+	return false;
+}
 }
