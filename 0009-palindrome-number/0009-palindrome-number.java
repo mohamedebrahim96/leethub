6d5d02ced7e8d87d9b86e4cbb8b98dcc1d41a0ec
Mohamed Ebrahim <ebrahimm131@gmail.com>
2023-05-13 17:49:49 +0400

    Time: 9 ms (99.41%), Space: 42.1 MB (49.98%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,20 +1,11 @@
-import java.lang.Integer;
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x < 0) {
-            return false;
+        if(x<0 || x!=0 && x%10 ==0 ) return false;
+        int check=0;
+        while(x>check){
+            check = check*10 + x%10;
+            x/=10;
         }
-        int r = x;
-        int finalVal = 0;
-        while (r != 0) {
-            int remainder = r % 10;
-            finalVal = finalVal * 10;
-            finalVal = finalVal + remainder;
-            r = r / 10;
-      }
-        if (x == finalVal) {
-          return true;
-        }
-    return false;
+        return (x==check || x==check/10);
     }
 }
