edb01d15307324aa4748fdb9e1b89b95646f628c
Mohamed Ebrahim <ebrahimm131@gmail.com>
2023-05-15 09:22:52 +0400

    Time: 2 ms (58.52%), Space: 46.2 MB (98.88%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,20 +1,20 @@
-import java.util.Arrays; 
-class Solution {
-    public int[] countBits(int num) {
-        int[] result = new int[num + 1];
-        Arrays.fill(result, 0);
-
-        for(int i = 1; i <= num; i++) {            
-            int val = i;
-            int count = 0;
-            while(val > 0) {
-                int isOne = val & 1;
-                count += isOne;
-                val = val >>> 1;
-            }
-            result[i] = count;
-        }
-        
-        return result;
-    }
+public class Solution {
+public int[] countBits(int num) {
+int[] arr = new int[num+1];
+for(int i=0; i<num+1;i++)
+{
+arr[i]=i;
+}
+int[] count = new int[num+1];
+count[0] = 0;
+for(int i=1; i<num+1;i++)
+{
+while(arr[i]!=0)
+{
+arr[i] = arr[i] & (arr[i] - 1);
+count[i]++;
+}
+}
+return count;
+}
 }
