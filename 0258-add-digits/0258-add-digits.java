b678b4c781b9db1d0c51e5dc5a202a70c11bc0c6
Mohamed Ebrahim <ebrahimm131@gmail.com>
2023-05-18 11:31:24 +0400

    Time: 1 ms (100.00%), Space: 40.2 MB (20.78%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,6 +1,18 @@
-class Solution {
-    public int addDigits(int num) 
-    {
-        return (num - 1) % 9 + 1;
+class Solution
+{
+    public int addDigits(int n)
+	{
+        while(n > 9)
+        {
+            int sum = 0;
+            
+            while(n > 0)
+            {
+                sum += n % 10;
+                n /= 10;
+            }
+            n = sum;
+        }    
+        return n;
     }
 }
