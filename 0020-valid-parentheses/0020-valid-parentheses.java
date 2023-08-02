e59956b2113ea088a2e2be1542c624043ffbe2e7
Mohamed Ebrahim <ebrahimm131@gmail.com>
2023-08-02 13:24:16 +0400

    Time: 2 ms (91.44%), Space: 41 MB (9.66%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,16 +1,37 @@
+import java.util.Stack;
+
 class Solution {
     public boolean isValid(String s) {
-	Stack<Character> stack = new Stack<Character>();
-	for (char c : s.toCharArray()) {
-		if (c == '(')
-			stack.push(')');
-		else if (c == '{')
-			stack.push('}');
-		else if (c == '[')
-			stack.push(']');
-		else if (stack.isEmpty() || stack.pop() != c)
-			return false;
-	}
-	return stack.isEmpty();
-}
+
+        Stack<Character> stack = new Stack<>();
+        char[] chars = s.toCharArray();
+
+        for (int i = 0; i < chars.length; i++) {
+            char newChar = chars[i];
+            if (chars[i] == '(' || chars[i] == '{' || chars[i] == '[') {
+                stack.push(chars[i]);
+            } else {
+
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                Character popped = stack.pop();
+                if (popped == '(') {
+                    if (newChar != ')') {
+                        return false;
+                    }
+                } else if (popped == '{') {
+                    if (newChar != '}') {
+                        return false;
+                    }
+                } else if (popped == '[') {
+                    if (newChar != ']') {
+                        return false;
+                    }
+                }
+            }
+        }
+        return stack.isEmpty();
+
+    }
 }
