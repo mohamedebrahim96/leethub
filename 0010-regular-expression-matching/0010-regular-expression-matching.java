02253d01746be690bb0b70ff3cb57d79160558e6
Mohamed Ebrahim <ebrahimm131@gmail.com>
2023-08-15 11:00:39 +0400

    Time: 1119 ms (8.95%), Space: 44.3 MB (6.34%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -1,5 +1,5 @@
 class Solution {
-   public boolean isMatch(String text, String pattern) {
+    public boolean isMatch(String text, String pattern) {
         if (pattern.isEmpty()) return text.isEmpty();
         boolean first_match = (!text.isEmpty() &&
                                (pattern.charAt(0) == text.charAt(0) || pattern.charAt(0) == '.'));
