a394381d3a0482eefb36bb060fcd5b24bd4c0c30
Mohamed Ebrahim <ebrahimm131@gmail.com>
2024-01-11 10:44:28 +0400

    Time: 1 ms (91.07%), Space: 41.9 MB (27.12%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,31 +1,24 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> triangle = new ArrayList<List<Integer>>();
-
-        // Base case; first row is always [1].
-        triangle.add(new ArrayList<>());
-        triangle.get(0).add(1);
-
-        for (int rowNum = 1; rowNum < numRows; rowNum++) {
-            List<Integer> row = new ArrayList<>();
-            List<Integer> prevRow = triangle.get(rowNum-1);
-
-            // The first row element is always 1.
-            row.add(1);
-
-            // Each triangle element (other than the first and last of each row)
-            // is equal to the sum of the elements above-and-to-the-left and
-            // above-and-to-the-right.
-            for (int j = 1; j < rowNum; j++) {
-                row.add(prevRow.get(j-1) + prevRow.get(j));
-            }
-
-            // The last row element is always 1.
-            row.add(1);
-
-            triangle.add(row);
+        if (numRows == 0) return new ArrayList<>();
+        if (numRows == 1) {
+            List<List<Integer>> result = new ArrayList<>();
+            result.add(Arrays.asList(1));
+            return result;
         }
-
-        return triangle;
+        
+        List<List<Integer>> prevRows = generate(numRows - 1);
+        List<Integer> newRow = new ArrayList<>();
+        
+        for (int i = 0; i < numRows; i++) {
+            newRow.add(1);
+        }
+        
+        for (int i = 1; i < numRows - 1; i++) {
+            newRow.set(i, prevRows.get(numRows - 2).get(i - 1) + prevRows.get(numRows - 2).get(i));
+        }
+        
+        prevRows.add(newRow);
+        return prevRows;
     }
 }
