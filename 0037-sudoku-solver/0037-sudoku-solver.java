c39ea25e4a754f53bf501a66c7388d0a42c1991a
Mohamed Ebrahim <ebrahimm131@gmail.com>
2024-09-20 09:15:33 +0400

    Time: 4 ms (93.80%), Space: 40.7 MB (83.26%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,33 +1,47 @@
 class Solution {
-
-    public boolean isValid(char[][] board, int row, int col, char c) {
-        for (int i = 0; i < 9; i++) {
-            if (board[i][col] == c) return false;
-            if (board[row][i] == c) return false;
-            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c) return false;
+    public void solveSudoku(char[][] board) {
+        backtrack(board,0);
+    }
+     public  boolean backtrack(char[][] sudoku, int position) {
+        if(position>=81){
+            return true;
         }
-        return true;
+        int row = position / 9;
+        int col = position % 9;
+        if(sudoku[row][col]=='.'){
+            for(int i=1;i<=9;i++){
+                if(isValid(sudoku,row,col,(char)('0'+i))){
+                    char c=(char)('0'+i);
+                    sudoku[row][col]=c;
+                    if(backtrack(sudoku,position+1)){
+                        return true;
+                    }
+                    sudoku[row][col]='.';
+                }
+            }
+            return false;
+        }
+        else{
+           return backtrack(sudoku,position+1);
+        }
+
     }
 
-    public boolean solve(char[][] board) {
+    public  boolean isValid(char[][] sudoku, int row, int col, char num) {
         for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (board[i][j] == '.') {
-                    for (char c = '1'; c <= '9'; c++) {
-                        if (isValid(board, i, j, c)) {
-                            board[i][j] = c;
-                            if (solve(board)) return true;
-                            else board[i][j] = '.';
-                        }
-                    }
+            if (sudoku[row][i] == num || sudoku[i][col] == num) {
+                return false;
+            }
+        }
+        int subrow=(row/3)*3;
+        int subcol=(col/3)*3;
+        for(int i=subrow;i<subrow+3;i++){
+            for(int j=subcol;j<subcol+3;j++){
+                if(sudoku[i][j]==num){
                     return false;
                 }
             }
         }
         return true;
-    }
-
-    public void solveSudoku(char[][] board) {
-        solve(board);
     }
 }
