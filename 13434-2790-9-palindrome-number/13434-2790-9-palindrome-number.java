edf7b76fe09f37fb17528d22ad8defa914a25d8f
Mohamed Ebrahim <ebrahimm131@gmail.com>
2025-01-10 09:03:01 +0400

    Time: 5 ms, Memory: 44.4 MB - LeetHub

---
+++ 13434-2790-9-palindrome-number/13434-2790-9-palindrome-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        int reverse = 0;
+        int xcopy = x;
+
+        while (x > 0) {
+            reverse = (reverse * 10) + (x % 10);
+            x /= 10;
+        }
+
+        return reverse == xcopy;        
+    }
+}
